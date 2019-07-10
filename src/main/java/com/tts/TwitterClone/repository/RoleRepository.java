7e65a9db381f9ec28bc3cd7ef7441e12013b32f8
Jordan Mor <jordanmor@gmail.com>
2019-07-10 10:49:18 -0400

    Add repositories for User and Role models

---
+++ src/main/java/com/tts/TwitterClone/repository/RoleRepository.java
@@ -0,0 +1,11 @@
+package com.tts.TwitterClone.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.tts.TwitterClone.model.Role;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long>{
+	Role findByRole(String role);
+}
